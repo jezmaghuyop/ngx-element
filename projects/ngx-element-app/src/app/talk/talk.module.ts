3ccf4b7a015f8eb18ed4f8e635bcf644533cd44f
Michael Krog <mic@apaq.dk>
2020-12-30 11:34:30 +0100

    Changes build

--- projects/ngx-element-app/src/app/talk/talk.module.ts
+++ projects/ngx-element-app/src/app/talk/talk.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, Type } from '@angular/core';
 import { TalkComponent } from './talk.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @NgModule({
   declarations: [TalkComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NzButtonModule, NzTagModule],
   exports: [TalkComponent],
   entryComponents: [TalkComponent]
 })
